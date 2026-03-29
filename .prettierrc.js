e90ac43fd2b6df3ac1ad6dac2d0bc285e90fae27
yogu-thinkpad <gonzalo.tajmuch@gmail.com>
2026-03-29 04:17:16 -0300

    app working

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ *
+ * If using prettierd, remember to call `prettierd restart` for the daemon to
+ * re-read the config file after changes
+ */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
+
+export default config
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,40 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { globSync } from "tinyglobby";
+import path from "node:path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+    plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+    build: {
+        rolldownOptions: {
+            // input: {
+            //     "/", "index.html",
+            //     ...Object.fromEntries(
+            //     globSync("src/*.").map((file) => [
+            //         // This removes `src/` as well as the file extension from each
+            //         // file, so e.g. src/nested/foo.js becomes nested/foo, and
+            //         // normalizes Windows backslashes to forward slashes.
+            //         path
+            //             .relative(
+            //                 "src",
+            //                 file.slice(
+            //                     0,
+            //                     file.length - path.extname(file).length
+            //                 )
+            //             )
+            //             .split(path.sep)
+            //             .join("/"),
+            //         // This expands the relative paths to absolute paths, so e.g.
+            //         // src/nested/foo.js becomes /project/src/nested/foo.js
+            //         path.resolve(file),
+            //     ])
+            // )},
+            // output: {
+            //     dir: 'dist',
+            //     format: 'esm',
+            // }, 
+        },
+    },
+});
